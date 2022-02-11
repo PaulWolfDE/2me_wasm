3cb9391d0a7f2f459c8952937e70a6f65ce5773a
Paul Wolf <Luqus@gmx.net>
2022-02-11 10:57:30 +0100

    Periodic Fraction Solver

---
+++ periodicfractionsolver/periodicfractionsolver.c
@@ -0,0 +1,62 @@
+/**
+ * periodicfractionsolver.c - C-Quelldatei
+ * ---------------------------------------
+ * Loest einen periodischen Dezimalbruch in einen gemeinen Bruch.
+ * 
+ * Uebersetzt aus Java, Original vom Oktober 2021, C-Variante vom Januar 2022.
+ * Paul Wolf, Januar 2022
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+
+#include "utils.h"
+
+#define MAX_ARR 256
+
+char* pfsSolve(char input[])
+{
+	printf("Length of '%s' = %lu\n", input, strlen(input));
+	for (int i = 0; i < strlen(input); i++)
+	{
+		if (input[i] == ',')
+		{
+			input[i] = '.';
+			break;
+		}
+	}
+
+	char *split1, *split2;
+	char del[] = "(", del2[] = ".";
+	split1 = strtok(input, del);
+	char *intVal = split1;
+	split1 = strtok(NULL, del);
+	char *perVal = split1;
+
+	double a = strtod(strtok(input, del), NULL);	// Dezimalzahl
+	int b = strtol(perVal, NULL, 10);				// Periode
+
+	split2 = strtok(intVal, del2);
+	split2 = strtok(NULL, del2);
+	char *decVal = split2;
+	perVal[strlen(perVal) - 1] = '\0';
+
+	int a2 = intVal[strlen(intVal) - 1] == '.' ? 0 : strlen(decVal);// Nachkommastellen
+	int b2 = strlen(perVal);								// Periodenstellen
+
+	long int r1 = a * pow(10, a2) * (pow(10, b2) - 1);
+	long int r2 = (long int) ((pow(10, b2) - 1) * pow(10, a2));
+	r1 += b;
+
+	int greatesCommonDivisor = gcd(r1, (int) r2);
+
+	static char r[MAX_ARR];
+
+	snprintf(r, sizeof(r), "%ld / %ld\n", r1 / greatesCommonDivisor,
+			r2 / greatesCommonDivisor);
+
+	return r;
+}
+// emcc PeriodicFractionSolver.c Utils.c -o PeriodicFractionSolver.js -s EXPORTED_FUNCTIONS="['_pfsSolve']" -s EXPORTED_RUNTIME_METHODS="['cwrap']" -s WASM=1
